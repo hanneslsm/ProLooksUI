5e4ab39436071e32e6ae560c9ed247c1f6d6b14a
hanneslsm <HannesLeismann@gmail.com>
2023-07-26 08:06:22 +0200

    Move patterns to Temp Templates

---
+++ patterns/wp-custom-template-temp-headers.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * Title: wp-custom-template-temp-headers
+ * Slug: prolooks/wp-custom-template-temp-headers
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|static-6","bottom":"var:preset|spacing|static-6"}}},"backgroundColor":"primary-20","textColor":"primary-110","layout":{"type":"constrained"},"fontSize":"paragraph-sm"} -->
+<div class="wp-block-group has-primary-110-color has-primary-20-background-color has-text-color has-background has-paragraph-sm-font-size" style="padding-top:var(--wp--preset--spacing--static-6);padding-bottom:var(--wp--preset--spacing--static-6)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Sign up and get 10% discount!</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-fill","fontSize":"info-md"} -->
+<div class="wp-block-button has-custom-font-size is-style-fill has-info-md-font-size"><a class="wp-block-button__link wp-element-button">Action</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+<div class="wp-block-group alignwide"><!-- wp:search {"label":"Search","showLabel":false,"buttonText":"Search","buttonPosition":"button-only","buttonUseIcon":true,"isSearchFieldHidden":true,"className":"hide-on-mobile"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"bottom":{"color":"var:preset|color|neutral-60","width":"1px"}},"spacing":{"blockGap":"var:preset|spacing|static-6"}},"backgroundColor":"light","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-light-background-color has-background" style="border-bottom-color:var(--wp--preset--color--neutral-60);border-bottom-width:1px"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
+<div class="wp-block-group alignwide"><!-- wp:site-logo {"width":200} /-->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"ref":126,"icon":"menu","align":"wide","className":"is-style-default","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"right"},"style":{"spacing":{"blockGap":"var:preset|spacing|static-16"}}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"bottom":{"color":"var:preset|color|neutral-60","width":"1px"}}},"backgroundColor":"light","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-light-background-color has-background" style="border-bottom-color:var(--wp--preset--color--neutral-60);border-bottom-width:1px"><!-- wp:site-logo {"width":200,"align":"center"} /-->
+
+<!-- wp:navigation {"ref":126,"icon":"menu","align":"wide","layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"blockGap":"var:preset|spacing|static-16"}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"bottom":{"color":"var:preset|color|neutral-60","width":"1px"}}},"backgroundColor":"light","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-light-background-color has-background" style="border-bottom-color:var(--wp--preset--color--neutral-60);border-bottom-width:1px"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:site-logo {"width":200} /-->
+
+<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+<div class="wp-block-group alignwide"><!-- wp:navigation {"ref":126,"icon":"menu","align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"right","orientation":"horizontal"},"style":{"spacing":{"blockGap":"var:preset|spacing|static-16"}}} /-->
+
+<!-- wp:buttons {"className":"hide-on-mobile"} -->
+<div class="wp-block-buttons hide-on-mobile"><!-- wp:button {"className":"is-style-fill nowrap"} -->
+<div class="wp-block-button is-style-fill nowrap"><a class="wp-block-button__link wp-element-button">Action</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/adam-davis-4mlWDd_zezI-unsplash-scaled.jpg","id":663,"minHeight":50,"minHeightUnit":"vh","customGradient":"linear-gradient(0deg,rgb(255,255,255) 0%,rgba(0,0,0,0.33) 18%)","contentPosition":"top center","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover has-custom-content-position is-position-top-center" style="min-height:50vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim wp-block-cover__gradient-background has-background-gradient" style="background:linear-gradient(0deg,rgb(255,255,255) 0%,rgba(0,0,0,0.33) 18%)"></span><img class="wp-block-cover__image-background wp-image-663" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/adam-davis-4mlWDd_zezI-unsplash-scaled.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"border":{"bottom":{"color":"var:preset|color|opacity-25","width":"1px"}},"spacing":{"padding":{"top":"var:preset|spacing|static-8","bottom":"var:preset|spacing|static-8"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="border-bottom-color:var(--wp--preset--color--opacity-25);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--static-8);padding-bottom:var(--wp--preset--spacing--static-8)"><!-- wp:group {"align":"wide","textColor":"light","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide has-light-color has-text-color"><!-- wp:site-logo {"width":200} /-->
+
+<!-- wp:navigation {"ref":126,"icon":"menu","align":"wide","className":"is-style-navigation-opacity","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"right","orientation":"horizontal"},"style":{"spacing":{"blockGap":"var:preset|spacing|static-16"}}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide"><!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"600","letterSpacing":"1px"}},"textColor":"primary-60","fontSize":"paragraph-sm"} -->
+<p class="has-text-align-center has-primary-60-color has-text-color has-paragraph-sm-font-size" style="font-style:normal;font-weight:600;letter-spacing:1px;text-transform:uppercase">everything you need</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":"1.2"}},"fontSize":"display-md"} -->
+<h2 class="wp-block-heading has-text-align-center has-display-md-font-size" style="line-height:1.2">The easiest way to build websites with Gutenberg</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Lorem ipsum dolor sit amet. Et repellat quia aut ullam autem aut minima dolor. Nam ipsum laboriosam et excepturi dolorsum rem neque internos. Qui aperiam eveniet At officia illo in labore dolor aut explicabo quaerat et vero voluptatem in tempore aspernatur. Eos assumenda officiis sed vero soluta et sint fugiat ut galisum autem ut voluptatum illum.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Get started →</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-button-secondary"} -->
+<div class="wp-block-button is-style-button-secondary"><a class="wp-block-button__link wp-element-button">Learn more →</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer {"height":"200px"} -->
+<div style="height:200px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/adam-davis-4mlWDd_zezI-unsplash-scaled.jpg","id":663,"minHeight":50,"minHeightUnit":"vh","customGradient":"linear-gradient(0deg,rgb(255,255,255) 0%,rgba(0,0,0,0.33) 18%)","contentPosition":"center center","style":{"spacing":{"padding":{"right":"0","left":"0","top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;min-height:50vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim wp-block-cover__gradient-background has-background-gradient" style="background:linear-gradient(0deg,rgb(255,255,255) 0%,rgba(0,0,0,0.33) 18%)"></span><img class="wp-block-cover__image-background wp-image-663" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/adam-davis-4mlWDd_zezI-unsplash-scaled.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"border":{"bottom":{"color":"var:preset|color|opacity-25","width":"1px"}},"spacing":{"padding":{"left":"clamp(16px,6vw,96px)","right":"clamp(16px,6vw,96px)","top":"var:preset|spacing|static-24","bottom":"var:preset|spacing|static-24"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="border-bottom-color:var(--wp--preset--color--opacity-25);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--static-24);padding-right:clamp(16px,6vw,96px);padding-bottom:var(--wp--preset--spacing--static-24);padding-left:clamp(16px,6vw,96px)"><!-- wp:group {"align":"full","textColor":"light","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignfull has-light-color has-text-color"><!-- wp:site-logo {"width":200} /-->
+
+<!-- wp:navigation {"ref":126,"icon":"menu","align":"wide","className":"is-style-navigation-opacity","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"right","orientation":"horizontal"},"style":{"spacing":{"blockGap":"var:preset|spacing|static-16"}}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"35vh"} -->
+<div style="height:35vh" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|fluid-16-96","left":"var:preset|spacing|fluid-16-96","top":"clamp(64px, 8vw, 96px)","bottom":"clamp(64px, 8vw, 96px)"}}},"layout":{"type":"constrained","justifyContent":"left","wideSize":"1000px","contentSize":"620px"}} -->
+<div class="wp-block-group alignfull" style="padding-top:clamp(64px, 8vw, 96px);padding-right:var(--wp--preset--spacing--fluid-16-96);padding-bottom:clamp(64px, 8vw, 96px);padding-left:var(--wp--preset--spacing--fluid-16-96)"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group alignfull"><!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"600","letterSpacing":"1px"}},"textColor":"primary-60","fontSize":"paragraph-sm"} -->
+<p class="has-text-align-center has-primary-60-color has-text-color has-paragraph-sm-font-size" style="font-style:normal;font-weight:600;letter-spacing:1px;text-transform:uppercase">This is prolooks</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"left","style":{"typography":{"lineHeight":"1.2"}},"fontSize":"display-md"} -->
+<h2 class="wp-block-heading has-text-align-left has-display-md-font-size" style="line-height:1.2">What if building websites with Wordpress was easy, fun and fast?</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"align":"left"} -->
+<p class="has-text-align-left">Introducing ProLooks Design. The core block, ready made solution for corporate websites, with mobile support and blazingly fast performance.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Get started →</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-button-secondary"} -->
+<div class="wp-block-button is-style-button-secondary"><a class="wp-block-button__link wp-element-button">Learn more →</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
