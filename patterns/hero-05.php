8856a7b1ac7c1df18a8e659e802f9158a43238e8
hanneslsm <HannesLeismann@gmail.com>
2023-08-11 13:06:00 +0200

    Create hero-05

---
+++ patterns/hero-05.php
@@ -0,0 +1,59 @@
+<?php
+ /**
+ * Title: Hero-05
+ * Slug: prolooksui/hero-05
+ * Categories: hero
+ * Viewport Width: 1280
+ * Inserter: true
+ * Since: 1.0
+ */
+?>
+
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|fluid-xxl","bottom":"var:preset|spacing|fluid-xxl"}}},"backgroundColor":"neutral-20","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-neutral-20-background-color has-background" style="padding-top:var(--wp--preset--spacing--fluid-xxl);padding-bottom:var(--wp--preset--spacing--fluid-xxl)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|fluid-xl"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":11,"sizeSlug":"large","linkDestination":"none","className":"is-style-image-hover-zoom"} -->
+<figure class="wp-block-image size-large is-style-image-hover-zoom"><img src="<?php echo esc_url( get_theme_file_uri('assets/images/placeholder-10.jpg') ); ?>" alt="" class="wp-image-11"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-right:0;padding-left:0"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|static-12"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"neutral-110"} -->
+<p class="has-neutral-110-color has-text-color" style="font-style:normal;font-weight:600">The Ultimate WordPress Block Theme: ProLooksUI</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":"1.2"}},"fontSize":"heading-1"} -->
+<h1 class="wp-block-heading has-heading-1-font-size" style="line-height:1.2">The easiest way to build beautiful Wordpress websites</h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"fontSize":"paragraph-lg"} -->
+<p class="has-paragraph-lg-font-size">Simple &amp; clean, beautiful &amp; easy to use.<br>Packed with multiple features and patterns. Building beautiful websites has never been easier! This is ProLooksUI.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group"><!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#">Get Started →</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-button-secondary"} -->
+<div class="wp-block-button is-style-button-secondary"><a class="wp-block-button__link wp-element-button" href="#">Learn more -&gt;</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:paragraph {"fontSize":"paragraph-sm"} -->
+<p class="has-paragraph-sm-font-size"><mark style="background-color:rgba(0, 0, 0, 0);color:#ff9500" class="has-inline-color">★★★★★</mark> <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-neutral-90-color">from 100+ reviews</mark></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
