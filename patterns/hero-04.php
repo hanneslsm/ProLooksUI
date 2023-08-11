71c6d1fafb0ba29819bac5885ea982516e49fbf9
hanneslsm <HannesLeismann@gmail.com>
2023-08-11 12:07:03 +0200

    Create hero-04.php

---
+++ patterns/hero-04.php
@@ -0,0 +1,54 @@
+<?php
+ /**
+ * Title: Hero-04
+ * Slug: prolooksui/hero-04
+ * Categories: hero
+ * Viewport Width: 1280
+ * Inserter: true
+ * Since: 1.0
+ */
+?>
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-right:0;padding-left:0"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|static-12"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary-110","fontSize":"paragraph-sm"} -->
+<p class="has-primary-110-color has-text-color has-paragraph-sm-font-size" style="font-style:normal;font-weight:600">The Ultimate WordPress Block Theme: ProLooksUI</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":"1.2"}},"fontSize":"heading-1"} -->
+<h1 class="wp-block-heading has-heading-1-font-size" style="line-height:1.2">The easiest way to build beautiful Wordpress websites</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"heading-6"} -->
+<p class="has-heading-6-font-size">Simple &amp; clean, beautiful &amp; easy to use.<br>Packed with multiple features and patterns. Building beautiful websites has never been easier! This is ProLooksUI.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group"><!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#">Get Started →</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-button-secondary"} -->
+<div class="wp-block-button is-style-button-secondary"><a class="wp-block-button__link wp-element-button" href="#">Learn more -&gt;</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:paragraph {"fontSize":"paragraph-sm"} -->
+<p class="has-paragraph-sm-font-size"><mark style="background-color:rgba(0, 0, 0, 0);color:#ff9500" class="has-inline-color">★★★★★</mark> <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-neutral-90-color">from 100+ reviews</mark></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":11,"sizeSlug":"large","linkDestination":"none","className":"is-style-image-picture-frame"} -->
+<figure class="wp-block-image size-large is-style-image-picture-frame"><img src="<?php echo esc_url( get_theme_file_uri('assets/images/placeholder-05.jpg') ); ?>" alt="" class="wp-image-11"/><figcaption class="wp-element-caption">This is a caption</figcaption></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
