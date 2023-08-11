e73f4e04082ed1df769abc3f5bee5a99240005b4
hanneslsm <HannesLeismann@gmail.com>
2023-08-11 11:48:02 +0200

    Create hero-03.php

---
+++ patterns/hero-03.php
@@ -0,0 +1,44 @@
+<?php
+ /**
+ * Title: Hero-03
+ * Slug: prolooksui/hero-03
+ * Categories: hero
+ * Viewport Width: 1280
+ * Inserter: true
+ * Since: 1.0
+ */
+?>
+
+<!-- wp:cover {"url":"<?php echo esc_url( get_theme_file_uri('assets/images/placeholder-01.jpg') ); ?>","id":10,"minHeight":80,"minHeightUnit":"vh","gradient":"cover","align":"full","style":{"spacing":{"padding":{"left":"0","right":"0"}}},"textColor":"light","layout":{"type":"default"}} -->
+<div class="wp-block-cover alignfull has-light-color has-text-color" style="padding-right:0;padding-left:0;min-height:80vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim wp-block-cover__gradient-background has-background-gradient has-cover-gradient-background"></span><img class="wp-block-cover__image-background wp-image-10" alt="" src="<?php echo esc_url( get_theme_file_uri('assets/images/placeholder-01.jpg') ); ?>" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:spacer {"height":"35vh"} -->
+<div style="height:35vh" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|fluid-lg","padding":{"top":"var:preset|spacing|fluid-3-xl","bottom":"var:preset|spacing|fluid-3-xl","left":"var:preset|spacing|fluid-16-96","right":"var:preset|spacing|fluid-16-96"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--fluid-3-xl);padding-right:var(--wp--preset--spacing--fluid-16-96);padding-bottom:var(--wp--preset--spacing--fluid-3-xl);padding-left:var(--wp--preset--spacing--fluid-16-96)"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"1px"}},"textColor":"primary-60","fontSize":"paragraph-md"} -->
+<p class="has-primary-60-color has-text-color has-paragraph-md-font-size" style="font-style:normal;font-weight:600;letter-spacing:1px">The Ultimate WordPress Block Theme: ProLooksUI</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"typography":{"lineHeight":"1.2"}},"fontSize":"display-md"} -->
+<h2 class="wp-block-heading has-display-md-font-size" style="line-height:1.2">The best way to build beautiful WordPress websites!</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet. Et repellat quia aut ullam autem aut minima dolor. Nam ipsum laboriosam et excepturi dolorsum rem neque internos. Qui aperiam eveniet At officia illo in labore dolor aut explicabo quaerat et vero voluptatem in tempore aspernatur. Eos assumenda officiis sed vero soluta et sint fugiat ut galisum autem ut voluptatum illum.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Get started →</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-button-secondary"} -->
+<div class="wp-block-button is-style-button-secondary"><a class="wp-block-button__link wp-element-button">Learn more →</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
